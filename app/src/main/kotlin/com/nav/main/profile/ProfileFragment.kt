c3fc86e1dc53f97dfdddf01633831b0066cfcd98
Maksym <m5732005@gmail.com>
2021-12-02 23:27:23 +0200

    [Profile Details] Navigate to external fragment.

--- app/src/main/kotlin/com/nav/main/profile/ProfileFragment.kt
+++ app/src/main/kotlin/com/nav/main/profile/ProfileFragment.kt
@@ -3,9 +3,12 @@ package com.nav.main.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.nav.R
 import com.nav.databinding.FragmentProfileBinding
+import com.nav.main.MainFragmentDirections
 
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
@@ -23,5 +26,15 @@ class ProfileFragment: Fragment(R.layout.fragment_profile) {
         btnDetails.setOnClickListener {
             findNavController().navigate(ProfileFragmentDirections.pushToProfileDetails())
         }
+
+        btnExternalDetails.setOnClickListener {
+            findTopNavController().navigate(MainFragmentDirections.pushToExternalProfileDetails())
+        }
+    }
+
+    private fun Fragment.findTopNavController(): NavController {
+        val topLevelFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.main_container)
+        val topLevelHost = topLevelFragment as NavHostFragment?
+        return topLevelHost?.navController ?: throw IllegalStateException("Top controller is not found")
     }
 }
